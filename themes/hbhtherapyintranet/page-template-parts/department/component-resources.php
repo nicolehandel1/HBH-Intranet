1e0938d1e6597aaeb4758cb989bde6b8ca1f04ff
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-09-29 21:40:51 -0400

    department commit

---
+++ themes/hbhtherapyintranet/page-template-parts/department/component-resources.php
@@ -0,0 +1,97 @@
+<style>
+.employee-grid-item {
+    width: 25%;
+    margin: 3%;
+}    
+.hb-sbt{
+     color: #4D4D4D;   
+    }
+.hb-cat {
+    display: inline;
+    background: #fff;
+    color: #76559A;
+    border: solid 1px #76559A;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    padding: 4px 7px 2px 7px;
+    margin: 0 10px;
+    border-radius: 4px;
+}
+    
+</style>
+
+<?php 
+// Updates
+?>
+
+<p class="clinician-subtitle" style="color: #76559A;"><?php the_field( 'updates_title' ); ?></p>
+<hr>
+<div>
+    <?php if ( have_rows( 'hr_article' ) ) :  while ( have_rows( 'hr_article' ) ) : the_row(); 
+		 $article = get_sub_field( 'article' ); if ( $article ) : 
+			 $post = $article; 
+			 setup_postdata( $post ); ?>
+
+    <div class="article-grid" data-category="transition">
+        <div class="article-grid-item-wrap">
+
+            <a href="<?php the_permalink(); ?>">
+                <h4 class="news-title"><?php the_title() ?></h4>
+            </a>
+            <p class="news-date"><?php echo get_the_date( 'F j, Y' ); ?></p>
+            <p class=""><?php the_field( 'single_excerpt_summary' ); ?> <a class="read-more" href="<?php the_permalink(); ?>">[READ MORE]</a></p>
+
+        </div>
+    </div>
+
+    <?php wp_reset_postdata();  endif;  endwhile;  else : ?>
+    <p>You are all up-to-date!</p>
+    <?php endif; ?>
+
+</div>
+
+<?php 
+// Team
+?>
+
+<p class="clinician-subtitle" style="margin-top: 50px;"><?php the_field( 'team_section_title' ); ?></p>
+<hr>
+
+<div class="" style="display: flex; flex-wrap: wrap;">
+
+    <?php if ( have_rows( 'team_member' ) ) : ?>
+    <?php while ( have_rows( 'team_member' ) ) : the_row(); ?>
+    <?php $employee = get_sub_field( 'employee' ); ?>
+    <?php if ( $employee ) : ?>
+    
+    <div class="employee-grid-item">
+        <div class="hdshot-wrap" style="background-image: url('<?php the_field( 'headshot', $employee ); ?>')"></div>
+        <a class="author" href="<?php echo get_author_posts_url($employee->ID); ?>"><?php echo $employee->display_name; ?></a>
+        <p class="job-title"><?php the_field( 'job_title', $employee); ?></p>
+        <a class="user-email" href=""><?php echo $employee->user_email; ?></a>
+    </div>
+    
+    <?php endif; ?>
+    <?php endwhile; ?>
+    <?php else : ?>
+    <?php // No rows found ?>
+    <?php endif; ?>
+</div>
+
+<?php 
+// Clinical Resources
+?>
+
+<p class="clinician-subtitle" style="margin-top: 50px;"><?php the_field( 'handebook_section_title' ); ?></p>
+<hr>
+
+<?php if ( have_rows( 'opening_section' ) ) : while ( have_rows( 'opening_section' ) ) : the_row(); ?>
+    <h4><?php the_sub_field( 'subheading' ); ?></h4>
+    <p><?php the_sub_field( 'content' ); ?></p>
+<?php endwhile; endif; ?>
+
+<?php if ( have_rows( 'closing_section' ) ) :  while ( have_rows( 'closing_section' ) ) : the_row(); ?>
+    <h4><?php the_sub_field( 'subheading' ); ?></h4>
+    <p><?php the_sub_field( 'content' ); ?></p>
+<?php endwhile;  endif; ?>
---
+++ themes/hbhtherapyintranet/page-template-parts/department/component-sidebar.php
@@ -0,0 +1,102 @@
+<?php 
+// Updates
+?>
+
+<p class="clinician-subtitle" style="margin-top: 0px;"><?php the_field( 'director_section_title' ); ?></p>
+<hr>
+
+<?php 
+    $author = get_field( 'employee' ); 
+?>
+
+<?php if ( $author ) : ?>
+
+    <div class="hdshot-wrap" style="background-image: url('<?php the_field( 'headshot', $author ); ?>')"></div>
+    <a class="author" href="<?php echo get_author_posts_url($author->ID); ?>" ><?php echo $author->display_name; ?></a>
+    <p class="job-title"><?php the_field( 'job_title', $author); ?></p>
+    <a class="user-email" href="" ><?php echo $author->user_email; ?></a>
+<?php endif; ?>
+
+<?php 
+// Buttons
+?>
+
+<p class="clinician-subtitle"><?php the_field( 'benefits_section_title' ); ?></p>
+<hr>
+
+<?php if ( have_rows( 'button' ) ): ?>
+	<?php while ( have_rows( 'button' ) ) : the_row(); ?>
+
+		<?php if ( get_row_layout() == 'internal_button' ) : ?>
+			<?php if ( have_rows( 'internal_page_link' ) ) : ?>
+				<?php while ( have_rows( 'internal_page_link' ) ) : the_row(); ?>
+					<?php $ip_link = get_sub_field( 'ip_link' ); ?>
+					<?php if ( $ip_link ) : ?>
+                        <a class="hr-btn btn" href="<?php echo esc_url( $ip_link); ?>" target="_self"><?php the_sub_field( 'label' ); ?></a>
+					<?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+		<?php elseif ( get_row_layout() == 'pdf_button' ) : ?>
+			<?php if ( have_rows( 'pdf_link' ) ) : ?>
+				<?php while ( have_rows( 'pdf_link' ) ) : the_row(); ?>
+                    <a class="hr-btn btn" href="<?php the_sub_field( 'pdf_link' ); ?>" target="_blank"><?php the_sub_field( 'pdf_label' ); ?></a>
+				<?php endwhile; ?>
+			<?php endif; ?>
+
+		<?php elseif ( get_row_layout() == 'ext_button' ) : ?>
+			<?php if ( have_rows( 'external_link' ) ) : ?>
+				<?php while ( have_rows( 'external_link' ) ) : the_row(); ?>
+                    <a class="hr-btn btn" href="<?php the_sub_field( 'ext_link' ); ?>" target="_blank"><?php the_sub_field( 'ext_label' ); ?></a>
+				<?php endwhile; ?>
+			<?php endif; ?>
+            
+        <?php elseif ( get_row_layout() == 'logo_button' ) : ?>
+			<?php if ( have_rows( 'logo_link' ) ) : ?>
+				<?php while ( have_rows( 'logo_link' ) ) : the_row(); ?>
+					<?php if ( get_sub_field( 'logo' ) ) : ?>
+                    <a class="hr-btn btn" href="<?php the_sub_field( 'logo_link' ); ?>"><img src="<?php the_sub_field( 'logo' ); ?>" /></a>
+					<?php endif ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+                        
+		<?php endif; ?>
+	<?php endwhile; ?>
+<?php else: ?>
+	<?php // No layouts found ?>
+<?php endif; ?>
+
+
+<p class="clinician-subtitle"><?php the_field( 'training_section_title' ); ?></p>
+<hr>
+
+<?php if ( have_rows( 'training_button' ) ): ?>
+	<?php while ( have_rows( 'training_button' ) ) : the_row(); ?>
+		<?php if ( get_row_layout() == 'internal_button' ) : ?>
+			<?php if ( have_rows( 'internal_page_link' ) ) : ?>
+				<?php while ( have_rows( 'internal_page_link' ) ) : the_row(); ?>
+					<?php $ip_link = get_sub_field( 'ip_link' ); ?>
+					<?php if ( $ip_link ) : ?>
+						<a class="hr-btn btn" href="<?php echo esc_url( $ip_link); ?>" target="_self"><?php the_sub_field( 'label' ); ?></a>
+					<?php endif; ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		<?php elseif ( get_row_layout() == 'pdf_button' ) : ?>
+			<?php if ( have_rows( 'pdf_link' ) ) : ?>
+				<?php while ( have_rows( 'pdf_link' ) ) : the_row(); ?>
+					<?php if ( get_sub_field( 'pdf_link' ) ) : ?>
+						<a class="hr-btn btn" href="<?php the_sub_field( 'pdf_link' ); ?>" target="_blank"><?php the_sub_field( 'pdf_label' ); ?></a>
+					<?php endif ?>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		<?php elseif ( get_row_layout() == 'ext_button' ) : ?>
+			<?php if ( have_rows( 'external_link' ) ) : ?>
+				<?php while ( have_rows( 'external_link' ) ) : the_row(); ?>
+					<a class="hr-btn btn" href="<?php the_sub_field( 'ext_link' ); ?>" target="_blank"><?php the_sub_field( 'ext_label' ); ?></a>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		<?php endif; ?>
+	<?php endwhile; ?>
+<?php else: ?>
+	<?php // No layouts found ?>
+<?php endif; ?>
