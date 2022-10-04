bc096fd5e3f446cf289957b897fa355213fdd5e7
nicolehandel1 <nicolehandel@hbhtherapy.com>
2022-10-04 17:19:13 -0400

    commit

--- themes/hbhtherapyintranet/post-template-parts/trainings/component-info.php
+++ themes/hbhtherapyintranet/post-template-parts/trainings/component-info.php
@@ -2,7 +2,20 @@
     <div class="section-content blog-info-contnt">
         
         <div class="single-content">
-            <?php the_field( 'section_content' ); ?>
+            
+            <?php if ( have_rows( 'content' ) ): ?>
+                <?php while ( have_rows( 'content' ) ) : the_row(); ?>
+                    <?php if ( get_row_layout() == 'subtitle' ) : ?>
+                        <h2><?php the_sub_field( 'training_subtitle' ); ?></h2>
+                    <?php elseif ( get_row_layout() == 'editor' ) : ?>
+                        <p><?php the_sub_field( 'training_editior' ); ?></p>
+                    <?php elseif ( get_row_layout() == 'video' ) : ?>
+                        <?php the_sub_field( 'training_editior' ); ?>
+                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <?php // No layouts found ?>
+            <?php endif; ?>
             
         </div>
         
