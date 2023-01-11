5d5ce88f941aec39c61b64de205e6d68d331e7be
nicolehandel1 <nicolehandel@hbhtherapy.com>
2023-01-11 16:07:22 -0500

    phone

--- themes/hbhtherapyintranet/post-template-parts/clinicians/component-info.php
+++ themes/hbhtherapyintranet/post-template-parts/clinicians/component-info.php
@@ -9,8 +9,8 @@ $author = get_queried_object();
             <p class="clinician-subtitle">Connect</p>
             <hr>
             
-            <a class="sd-email" href="mailto:<?php echo $author->user_email; ?>"><span class="dashicons dashicons-email-alt2"></span><?php echo $author->user_email; ?></a>
-            <p><?php the_field( 'phone_number', $user_id_prefixed ); ?></p>
+            <a class="sd-email" href="mailto:<?php echo $author->user_email; ?>"><span class="dashicons dashicons-email-alt2"></span> <?php echo $author->user_email; ?></a>
+            <p><span class="dashicons dashicons-phone"></span> <?php the_field( 'phone_number', $author ); ?></p>
 
             <a class="hr-btn btn logo-btn" href="<?php the_field( 'linkedin', $author ); ?>"><img src="http://intranet.hbhtherapy.com/wp-content/uploads/2022/09/Linkedin-logo-png.png" /></a>
 
